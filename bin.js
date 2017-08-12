3a76b024e6c0c9338063872802d97a3a9cb31e1c
Kyle E. Mitchell <kyle@kemitchell.com>
2017-08-11 20:09:32 -0700

    Add bin script

---
+++ bin.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+if (process.argv.length !== 7) {
+  process.stdout.write(
+    'Usage: <publisher> <password> <project> <edition> <digest>\n'
+  )
+  process.exit(1)
+}
+
+require('./')
+  .apply(null, process.argv.slice(2, 7), function (error, location) {
+    if (error) {
+      process.stderr.write(error.toString() + '\n')
+      process.exit(1)
+    } else {
+      process.stdout.write(location + '\n')
+      process.exit(0)
+    }
+  })
